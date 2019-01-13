66be63ae1c585b7ca05f122f1e8ab9809ca7df8b
Tim Müller-Seydlitz <timbms@gmail.com>
2019-01-13 17:35:56 +0100

    Some change

--- openHAB/NSObjectExtension.swift
+++ openHAB/NSObjectExtension.swift
@@ -10,18 +10,16 @@ import Foundation
 
 extension NSObject {
 
-
-
     func getProperties(from classType: NSObject.Type) -> [String] {
 
         var propertiesCount : CUnsignedInt = 0
         let propertiesInAClass = class_copyPropertyList(classType, &propertiesCount)
-        let propertiesArray : [String]
+        var propertiesArray : [String] = []
 
         for i in 0 ..< Int(propertiesCount) {
             let property = propertiesInAClass?[i]
             let strKey = NSString(utf8String: property_getName(property!)) as String?
-            propertiesArray.append(strKey)
+            propertiesArray.append(strKey!)
 
         }
         return propertiesArray
